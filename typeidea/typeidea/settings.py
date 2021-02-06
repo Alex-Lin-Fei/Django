d716bd5b7ffedc6e636583a137246bb6fb5003e8
Alex-Lin-Fei <2055017970@qq.com>
2021-02-06 16:55:27 +0800

    add blog

--- typeidea/typeidea/settings.py
+++ typeidea/typeidea/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'typeidea.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [TEMPLATES_DIR, ]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
